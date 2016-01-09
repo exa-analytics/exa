0ebaacc7ee9760aa07f68dce288461eb81292fa3
Alex Marchenko <alexvmarch@gmail.com>
2016-01-09 16:24:00 -0500

    Fixed missing _getitem in IsotopeMeta

--- exa/relational.py
+++ exa/relational.py
@@ -282,10 +282,15 @@ class IsotopeMeta(Meta):
         '''
         return session.query(self).filter(self.symbol == symbol).all()
 
-    def get_by_AZ(self, element):
+    def get_by_strid(self, element):
         '''
         '''
         return session.query(self).filter(self.strid == element).all()[0]
+
+    def get_szuid(self, number):
+        '''
+        '''
+        return session.query(self).filter(self.szuid == number).all()[0]
 
     def _getitem(self, key):
         if isinstance(key, str):
