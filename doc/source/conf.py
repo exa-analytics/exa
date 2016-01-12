d5a2c8c934f755133d6d513e38836315e9766431
Alex Marchenko <alexvmarch@gmail.com>
2016-01-12 14:15:12 -0500

    added back commented dependencies and Mock() for seaborn

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas', 'scipy', 'tables']
+MOCK_MODULES = ['argparse', 'numpy', 'pandas', 'scipy', 'tables', 'seaborn']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
