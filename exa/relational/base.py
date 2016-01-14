c4f0ce1713e226dd1d300d6cc80e5d5159eec179
Alex Marchenko <alexvmarch@gmail.com>
2016-01-14 14:35:20 -0500

    Non-scoped sessions for now

--- exa/relational/base.py
+++ exa/relational/base.py
@@ -117,5 +117,6 @@ def create_all():
 engine_name = Config.relational_engine()
 engine = create_engine(engine_name)
 Session = sessionmaker(bind=engine)
+#TODO: SEE ISSUE #41  session = Session() vs session = scoped_session(sessionmaker(bind=engine))
+# For now, non-scoped
 session = Session()
-#session = scoped_session(sessionmaker(bind=engine))
