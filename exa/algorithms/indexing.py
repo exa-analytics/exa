19963bebda5df62823566de8cd4245d3f96d3d15
Alex Marchenko <alexvmarch@gmail.com>
2016-02-28 22:15:48 -0500

    smaller in memory index creation function

--- exa/algorithms/indexing.py
+++ exa/algorithms/indexing.py
@@ -27,6 +27,24 @@ def idxs_from_starts_and_counts(starts, counts):
     return (i_idx, j_idx, values)
 
 
+def idxs_from_starts_and_count(starts, count):
+    '''
+    '''
+    n = len(starts)
+    i_idx = np.empty((n * count, ), dtype='i8')
+    j_idx = i_idx.copy()
+    values = j_idx.copy()
+    h = 0
+    for i, start in enumerate(starts):
+        stop = start + count
+        for j, value in enumerate(range(start, stop)):
+            i_idx[h] = i
+            j_idx[h] = j
+            values[h] = value
+            h += 1
+    return (i_idx, j_idx, values)
+
+
 def _unordered_pairing_function(x, y):
     '''
     http://www.mattdipasquale.com/blog/2014/03/09/unique-unordered-pairing-function/
