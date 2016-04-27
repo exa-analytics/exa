2cd169bddde93ae7ced4e26c2e618eeb6da9938e
Alex <alexvmarch@gmail.com>
2016-04-27 19:21:40 -0400

    Reworking utilities

--- exa/__init__.py
+++ exa/__init__.py
@@ -4,22 +4,7 @@ __version__ = '.'.join((str(v) for v in __exa_version__))
 
 import atexit as _ae
 
-# Sensible plotting defaults
-import seaborn as _sns
-_legend = {'legend.frameon': True, 'legend.facecolor': 'white',
-           'legend.edgecolor': 'black'}
-_mathtext = {'mathtext.default': 'rm'}
-_save = {'savefig.format': 'pdf', 'savefig.bbox': 'tight',
-         'savefig.transparent': True, 'savefig.pad_inches': 0.1,
-         'pdf.compression': 9}
-_rc = _legend
-_rc.update(_mathtext)
-_rc.update(_save)
-_sns.set(context='poster', style='white', palette='colorblind', font_scale=1.3,
-         font='serif', rc=_rc)
-
 from exa._config import _conf, show_conf
-from exa.utility import savefig
 from exa.log import log_names, log_head, log_tail
 from exa.test import run_doctests, run_unittests
 from exa import tests
