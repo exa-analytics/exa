6f3d8f477e5582e8e6521f834b6af84855bb29e6
Alex <alexvmarch@gmail.com>
2016-04-27 23:47:42 -0400

    Fixed some minor bugs in editor preventing passing of kwargs

--- exa/__init__.py
+++ exa/__init__.py
@@ -2,7 +2,9 @@
 __exa_version__ = (0, 2, 1)    # Version number is defined here!
 __version__ = '.'.join((str(v) for v in __exa_version__))
 
+
 import atexit as _ae
+
 
 from exa._config import _conf, show_conf
 from exa.log import log_names, log_head, log_tail
@@ -13,9 +15,16 @@ from exa.numerical import Series, DataFrame, Field3D, SparseDataFrame
 from exa.editor import Editor
 from exa import algorithms
 
+
 if not _conf['exa_persistent']:
     from exa._install import install
     install()
+
+
+if _conf['notebook']:
+    i = get_ipython()
+    c = i.config
+    c.InteractiveShellApp.matplotlib = 'inline'
 
 
 _ae.register(_config._cleanup)          # Register functions in opposite desired
