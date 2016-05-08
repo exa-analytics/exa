92761eabcb2e00e4630c870e2f6694f47b2a2337
Alex Marchenko <alexvmarch@gmail.com>
2016-05-07 21:18:54 -0400

    Rethinking jitted organization

--- exa/algorithms/__init__.py
+++ exa/algorithms/__init__.py
@@ -2,7 +2,6 @@
 from exa import _conf
 
 if _conf['pkg_numba']:
-    from exa.algorithms.jitted import vmag3, vdist3
     from exa.algorithms.jitted import (arange1, arange2, unordered_pairing,
                                        indexes_sc1, indexes_sc2)
     from exa.algorithms.jitted import pdist, supercell3d, meshgrid3d
