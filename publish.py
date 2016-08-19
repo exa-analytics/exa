63bed6c61b96099947c0dda9be22dc1a0af1e9ed
Alex Marchenko <alexvmarch@gmail.com>
2016-08-19 09:24:10 -0400

    Publication script - checks with the user before performing actions, requires python 3 to use (but can conda build for arbitrary).

--- publish.py
+++ publish.py
@@ -7,12 +7,17 @@ Build/Publish
 #################
 """
 import os
+import shutil
 import argparse
 import subprocess
 
 
+builds = ["win-32", "win-64", "osx-64", "linux-32", "linux-64"]
+
+
 def get_args():
     """
+    Description of the deployment script.
     """
     parser = argparse.ArgumentParser(description="Deployment script for Exa Analytics")
     parser.add_argument("pkg", nargs="?")
@@ -36,17 +41,28 @@ def get_package_version(pkg):
     Determine the package version from the primary __init__.py file.
     """
     search = "__{}_version__".format(pkg)
+    versions = []
     with open(os.sep.join((pkg, "__init__.py"))) as f:
         for line in f:
             if search in line:
                 v = line.split('=')[-1]
                 v = v.replace("(", "").replace(")", "").replace(",", " ")
-                return tuple((int(i) for i in v.split()))
-    raise Exception("Package version unknown")
+                versions.append(tuple((int(i) for i in v.split())))
+                break
+    with open("meta.yaml") as f:
+        for line in f:
+            if any(x in line for x in ["version:", "git_rev:"]):
+                v = line.split()[-1].replace('"', '').replace('v', '').split('.')
+                versions.append(tuple(int(i) for i in v))
+    if not all(version == versions[0] for version in versions):
+        raise Exception("Version mismatch between init {} and meta.yaml {}!".format(versions[0], versions[1]))
+    else:
+        return versions[0]
 
 
 def get_python_versions():
     """
+    Get versions of python to build for.
     """
     versions = []
     read = False
@@ -66,7 +82,8 @@ def check(pkg, version, pys):
     Human check for publication.
     """
     v = ".".join([str(i) for i in version])
-    py = ", ".join([py.replace("'", "") for py in ps])
+    py = ", ".join([py.replace("'", "") for py in pys])
+    print("Before deploying, make sure '{}' version {} has been tagged for release.".format(pkg, v))
     inp = "Deploy '{}' {} for python {} on all platforms (y/N): ".format(pkg, v, py)
     chk = input(inp)
     if chk.lower() == "y":
@@ -76,8 +93,8 @@ def check(pkg, version, pys):
 
 def conda_build(pys):
     """
+    Deploy to anaconda.
     """
-    builds = ["win-32", "win-64", "osx-64", "linux-32", "linux-64"]
     build_fmt = "conda build --python={py} ."
     conv_fmt = "conda convert {} -p all"
     conup_fmt = "anaconda upload {}/{}"
@@ -104,9 +121,20 @@ def conda_build(pys):
 
 def pypi_build(pypi):
     """
+    Deploy to pypi/pypitest.
     """
-    ret = subprocess.run(["python setup.py sdist upload pypi"], shell=True,
-                         check=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    string = "python setup.py sdist upload {}".format(pypi)
+    ret = subprocess.run([string], shell=True, check=True,
+                         stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+
+
+def cleanup():
+    """
+    Cleanup the build directories.
+    """
+    for build in builds:
+        if os.path.isdir(build):
+            shutil.rmtree(build)
 
 
 if __name__ == "__main__":
@@ -117,6 +145,7 @@ if __name__ == "__main__":
         args.version = get_package_version(args.pkg)
     pys = get_python_versions()
     chk = check(args.pkg, args.version, pys)
-    if check:
+    if chk:
         conda_build(pys)
         pypi_build(args.which_pypi)
+    cleanup()
