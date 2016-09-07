1411ae3a3a975b4005f3f79f82aef1ed0100860a
Alex Marchenko <alexvmarch@gmail.com>
2016-09-07 11:13:02 -0400

    Version labeled as 0.3.0.dev. This commit sets the stage for the v0.3 release doing a little bit of reorganization and code improvements. Docs has been renamed in order to move away from RTD dependence; instead docs are now automatically built by (travis) CI and deployed at https://[user or org].github.io/exa[tomic,nwchem,etc]. See for example https://avmarchenko.github.io/exa/. The docs are now much more flexible and can be written in md or rst format depending on user preference (note that recommonmark is required to build the docs - see changes in .travis.yml). At the root level organization has changed to include static files and demo notebooks in a systematic fashion.

--- exa/_version.py
+++ exa/_version.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2015-2016, Exa Analytics Development Team
 # Distributed under the terms of the Apache License 2.0
-version_info = (0, 3, 0, 'dev')
+version_info = (0, 3, 0, 'dev0')
 __version__ = '.'.join(map(str, version_info))
