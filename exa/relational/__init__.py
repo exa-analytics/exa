6bc10fd6d00593c3ad192a4bf528e9e01dd605c3
Alex Marchenko <alexvmarch@gmail.com>
2016-09-11 11:57:28 -0400

    Comment out relational for now; working on workflows, parallelism first

--- exa/relational/__init__.py
+++ exa/relational/__init__.py
@@ -7,7 +7,7 @@ Relational
 This (sub)package is provides the content management framework for container
 objects and a collection of static data for reference and unit conversions.
 """
-from exa.relational.isotope import Isotope
+#from exa.relational.isotope import Isotope
 #from exa.relational.constant import Constant
 #from exa.relational.unit import Length, Mass, Time, Current, Amount, Luminosity
 #from exa.relational.unit import Dose, Acceleration, Charge, Dipole, Energy
