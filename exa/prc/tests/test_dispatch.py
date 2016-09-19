74e7d38275a04c12ca1c6786b392c537473eafa6
Alex Marchenko <alexvmarch@gmail.com>
2016-09-19 08:33:15 -0400

    Start of tests for dispatch

--- exa/prc/tests/test_dispatch.py
+++ exa/prc/tests/test_dispatch.py
@@ -2,6 +2,13 @@
 # Copyright (c) 2015-2016, Exa Analytics Development Team
 # Distributed under the terms of the Apache License 2.0
 """
-Tests for :mod:`~exa.workflow.dispatch`
+Tests for :mod:`~exa.prc.dispatch`
 ##########################################
 """
+from exa.prc.dispatch import dispatch
+from exa.tester import UnitTester
+
+
+class TestDispatcher(UnitTester):
+    """
+    """
