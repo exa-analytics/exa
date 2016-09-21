97966e5bc3896497d546c8cded4dee31d4b1821f
Alex Marchenko <alexvmarch@gmail.com>
2016-09-21 17:29:57 -0400

    Updated dispatch tests

--- exa/compute/tests/test_dispatch.py
+++ exa/compute/tests/test_dispatch.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2015-2016, Exa Analytics Development Team
 # Distributed under the terms of the Apache License 2.0
 """
-Tests for :mod:`~exa.prc.dispatch`
+Tests for :mod:`~exa.compute.dispatch`
 ##########################################
 """
 import numpy as np
