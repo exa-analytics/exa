c1cab12f693469cd3e746832a6e51a38cdaa79d7
Alex Marchenko <alexvmarch@gmail.com>
2016-10-02 13:12:59 -0400

    Partial update of symbolic

--- exa/core/symbolic.py
+++ exa/core/symbolic.py
@@ -5,37 +5,22 @@
 Symbolic Data Objects
 ###################################
 This module provides a mechanism for working with symbolic data using `sympy`_.
-It extens
 
 .. _sympy: http://www.sympy.org/en/index.html
 """
 import six
+import sympy as sy
 from exa.core.typed import TypedMeta
 
-class SymbolicMeta(TypedMeta):
-    """
-    """
-    pass
-    
 
-class SymbolicFunction(six.with_metaclass(TypedMeta, sy.Function)):
-    """Base class for exa's symbolic function system."""
-    def recompile(self):
-        self._compiled =
+class Meta(TypedMeta):
+    """Metaclass for :class:`~exa.core.symbolic.SymbolicFunction`."""
+    _getter_prefix = "compile"
+    compiled = sy.Expr
 
 
-    def compiled(self):
-        """Compile the discrete function representation."""
-        if not hasattr("sel")
-        func = self.lambdify()
-        self._compiled = self.lambdify
-
-    def __call__(self, *args, **kwargs):
-        return self.compiled(*args, **kwargs)
-        f = self.compile_function
-        func = self.lambdify(recompile)
-        return func(*args, **kwargs)
-
+class Base(six.with_metaclass(Meta, sy.Function)):
+    """Base class for exa's symbolic data objects."""
     @classmethod
     def new_expression(cls, expression, *itypes, **kwargs):
         """
@@ -44,47 +29,17 @@ class SymbolicFunction(six.with_metaclass(TypedMeta, sy.Function)):
         Args:
             expression: Any valid sympy expression
         """
-
         expression.__class__._itypes = itypes
         expression.__class__.__call__ = cls.__call__
         return expression
-import sympy as sy
-from numba import vectorize, jit
 
-
-class SymbolicFunction(sy.Function):
-    """
-    Base class for symbolic functions.
-    """
-# This should use new compilation machinery
-#    def lambdify(self, recompile=False):
-#        """
-#        Compile the function for discrete evaluation.
-#        """
-#        if self._discrete_func is None or recompile == True:
-#            npfunc = sy.lambdify(sorted(self.free_symbols), self, 'numpy')
-#            if self._compile_type == "jit":
-#                self._discrete_func = jit(nopython=True)(npfunc)
-#            elif self._compile_type == "vectorize":
-#                self._discrete_func = vectorize([], nopython=True)(npfunc)
-#            else:
-#                raise NotImplementedError()
-#        return self._discrete_func
-
-    def __call__(self, *args, recompile=False, **kwargs):
+    def __call__(self, *args, **kwargs):
+        return self.compiled(*args, **kwargs)
+        f = self.compile_function
         func = self.lambdify(recompile)
         return func(*args, **kwargs)
 
-    @classmethod
-    def new_expression(cls, expression, compile_type="jit", signature=None):
-        """
-        Create a new expression instance.
 
-        Args:
-            expression: Any valid sympy expression
-            compile_type (str): One of "jit" or "vectorize" corresponding to numba compile type
-        """
-        expression.__class__._compile_type = compile_type
-        expression.__class__._signature = signature
-        expression.__class__.__call__ = cls.__call__
-        return expression
+class Function(Base):
+    """Symbolic functions"""
+    pass
