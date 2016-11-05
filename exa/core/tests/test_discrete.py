3b1ac674ebb069169834e56b63035c743c9314ba
Alex Marchenko <alexvmarch@gmail.com>
2016-11-04 23:12:41 -0400

    First test for series

--- exa/core/tests/test_discrete.py
+++ exa/core/tests/test_discrete.py
@@ -11,36 +11,13 @@ from exa.tester import UnitTester
 from exa.core.discrete import Series
 
 
-class CustomSeries(Series):
-    """Test subclassing of :class:`~exa.core.discrete.Series`."""
-    pass
-
-
 class TestSeries(UnitTester):
     """Tests for :class:`~exa.core.discrete.Series`."""
     def setUp(self):
-        pass
-        #self.series0 = Series(np.random.rand(10), metadata={"name": "s0"})
-        #self.series1 = Series(np.random.rand(10), metadata={"name": "s1"})
-        #self.series2 = CustomSeries(np.random.rand(10), metadata={"name": "s2"})
+        self.s0 = Series([1, 2, 3], units="kJ")
 
-    def test_copy(self):
-        """
-        Test that copying (:func:`~exa.core.discrete.Series.copy`) returns the
-        correct type.
-        """
-        #obj0 = self.series0.copy()
-        #obj1 = self.series2.copy()
-        #self.assertFalse(obj0 is self.series0)
-        #self.assertFalse(obj1 is self.series2)
-        #self.assertIsInstance(obj0, Series)
-        #self.assertIsInstance(obj1, CustomSeries)
-
-    def test_combine_const(self):
-        """Test metadata propagation through adding constants."""
-        #obj0 = self.series0 + 1
-        #obj1 = self.series1 + 1.0
-        #obj2 = self.series1*2.0
-        #self.assertEqual(obj0.metadata['name'], 's0')
-        #self.assertEqual(obj1.metadata['name'], 's1')
-        #self.assertEqual(obj2.metadata['name'], 's2')
+    def test_convert_units(self):
+        """Test :func:`~exa.core.discrete.Series.convert_units`."""
+        self.assertEqual(self.s0.sum(), 6)
+        self.s0.convert_units("J")
+        self.assertEqual(self.s0.sum(), 6000)
