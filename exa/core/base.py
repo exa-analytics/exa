96ec49c602a6dd1c4d4e6f23eddd08c900c10955
Alex Marchenko <alexvmarch@gmail.com>
2016-12-22 16:05:53 -0500

    Updated core Meta class (metaclass)

--- exa/core/base.py
+++ exa/core/base.py
@@ -46,24 +46,31 @@ class Meta(Typed):
     units = Unit
 
     @staticmethod
-    def modify_op(dunder_op):
-        """Modifies mathematical operations to support unit conversions."""
-        def wrapper(self, other, *args, **kwargs):
-            if isinstance(other, Meta) and self.units != other.units:
-                pass
-                #other = other.copy()*self.units/other.units
-            return getattr(super(mcs, self), dunder_op)(other, *args, **kwargs).__finalize__(self)
-        return wrapper
+    def modify_op(op):
+        def op_wrapper(self, other, *args, **kwargs):
+            return getattr(super(self.__class__, self), op)(other, *args, **kwargs).__finalize__(self)
+        return op_wrapper
+
+    #def modify_op(mcs, dunder_op):
+    #    """Modifies mathematical operations to support unit conversions."""
+    #    def wrapper(self, other, *args, **kwargs):
+    #        if isinstance(other, Meta) and self.units != other.units:
+    #            pass
+    #            #other = other.copy()*self.units/other.units
+    #        return getattr(mcs, dunder_op)(other, *args, **kwargs).__finalize__(self)
+    #    return wrapper
 
     def __finalize__(self, other, method=None, **kwargs):
-        """Propagate metadata."""
         for name in self._metadata:
             object.__setattr__(self, name, getattr(other, name, None))
         return self
 
     def __new__(mcs, name, bases, clsdict):
-        for op, info in _op_descriptions.items():
-            op_name = "__{}__".format(op)
-            clsdict[op_name] = mcs.modify_op(op_name)
+        for op_name, info in _op_descriptions.items():
+            op = "__{}__".format(op_name)
+            clsdict[op] = mcs.modify_op(op)
+            if info['reverse'] is not None:
+                op = "__{}__".format(info['reverse'])
+                clsdict[op] = mcs.modify_op(op)
         clsdict['__finalize__'] = mcs.__finalize__
         return super(Meta, mcs).__new__(mcs, name, bases, clsdict)
