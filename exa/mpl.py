c3841c77ad53c689be3ef47c418540d752bf309a
tjduigna <thomas.j.duignan@gmail.com>
2017-01-03 05:38:27 -0800

    Removed mpl figure subclass

--- exa/mpl.py
+++ exa/mpl.py
@@ -7,60 +7,97 @@ Matplotlib Utilities
 """
 import seaborn as sns
 # Subclassing matplotlib Figure and Axes
-from matplotlib.figure import Figure
-from matplotlib.axes import Axes, subplot_class_factory
+#from matplotlib.figure import Figure
+#from matplotlib.axes import Axes, subplot_class_factory
 #from matplotlib.backends.backend_agg import FigureCanvasAgg
 
-legend = {'legend.frameon': True, 'legend.facecolor': 'white',
-          'legend.fancybox': True, 'patch.facecolor': 'white',
-          'patch.edgecolor': 'black'}
-axis = {'axes.formatter.useoffset': False}
-mpl_legend = {'legend.frameon': True, 'legend.facecolor': 'white',
-           'legend.edgecolor': 'black'}
-mpl_mathtext = {'mathtext.default': 'rm'}
-mpl_save = {'savefig.format': 'pdf', 'savefig.bbox': 'tight',
-         'savefig.transparent': True, 'savefig.pad_inches': 0.1,
-         'pdf.compression': 9}
-mpl_rc = mpl_legend
-mpl_rc.update(mpl_mathtext)
-mpl_rc.update(mpl_save)
-sns.set(context='poster', style='white', palette='colorblind', font_scale=1.3,
-        font='serif', rc=mpl_rc)
+import numpy as np
+#from mpl_toolkits.mplot3d import Axes3D
 
-def _stale_figure_callback(cls, val):
-    if cls.figure:
-        cls.figure.stale = val
+#legend = {'legend.frameon': True, 'legend.facecolor': 'white',
+#          'legend.fancybox': True, 'patch.facecolor': 'white',
+#          'patch.edgecolor': 'black'}
+#axis = {'axes.formatter.useoffset': False}
+#mpl_legend = {'legend.frameon': True, 'legend.facecolor': 'white',
+#           'legend.edgecolor': 'black'}
+#mpl_mathtext = {'mathtext.default': 'rm'}
+#mpl_save = {'savefig.format': 'pdf', 'savefig.bbox': 'tight',
+#         'savefig.transparent': True, 'savefig.pad_inches': 0.1,
+#         'pdf.compression': 9}
+#mpl_rc = mpl_legend
+#mpl_rc.update(mpl_mathtext)
+#mpl_rc.update(mpl_save)
+#sns.set(context='poster', style='white', palette='colorblind', font_scale=1.3,
+#        font='serif', rc=mpl_rc)
 
-class ExaFigure(Figure):
-    """A custom figure class that allows using custom Axes subclasses."""
-
-    def add_subplot(self, axis, *args, **kwargs):
-        """Override add_subplot to accept custom Axes objects."""
-        key = self._make_key(*args, **kwargs)
-        a = subplot_class_factory(axis)(self, *args, **kwargs)
-        self._axstack.add(key, a)
-        self.sca(a)
-        self.stale = True
-        a._remove_method = self._Figure__remove_ax
-        a.stale_callback = _stale_figure_callback
-        return a
-
-    def __init__(self, *args, **kwargs):
-        super(ExaFigure, self).__init__(*args, **kwargs)
-        print('Must fig.set_canvas(matplotlib.backends.'
-              'backend_agg.FigureCanvasAgg(self))')
+#def _stale_figure_callback(cls, val):
+#    if cls.figure:
+#        cls.figure.stale = val
+#
+#class ExaFigure(Figure):
+#    """A custom figure class that allows using custom Axes subclasses."""
+#
+#    def add_subplot(self, axis, *args, **kwargs):
+#        """Override add_subplot to accept custom Axes objects."""
+#        key = self._make_key(*args, **kwargs)
+#        a = subplot_class_factory(axis)(self, *args, **kwargs)
+#        self._axstack.add(key, a)
+#        self.sca(a)
+#        self.stale = True
+#        a._remove_method = self._Figure__remove_ax
+#        a.stale_callback = _stale_figure_callback
+#        return a
+#
+#    def __init__(self, *args, **kwargs):
+#        super(ExaFigure, self).__init__(*args, **kwargs)
+#        print('Must fig.set_canvas(matplotlib.backends.'
+#              'backend_agg.FigureCanvasAgg(self))')
 #        self.set_canvas(FigureCanvasAgg(self))
 
-class ExAxes(Axes):
-    """
-    A custom matplotlib Axes object for convenience methods related to
-    various dataframes.
+#class ExAxes(Axes):
+#    """
+#    A custom matplotlib Axes object for convenience methods related to
+#    various dataframes.
+#
+#    .. code-block:: Python
+#
+#        fig = ExaFigure()
+#        ax = fig.add_subplot(ExAxes, 111)
+#        ax.plot(x, y)
+#    """
+#    def __init__(self, *args, **kwargs):
+#        super(ExAxes, self).__init__(*args, **kwargs)
 
-    .. code-block:: Python
 
-        fig = ExaFigure()
-        ax = fig.add_subplot(ExAxes, 111)
-        ax.plot(x, y)
+def _gen_figure(nxplot=1, nyplot=1, joinx=False, joiny=False,
+                x=None, y=None, z=None, nxlabel=None, nylabel=None,
+                nzlabel=None, figargs=None, projection=None):
     """
-    def __init__(self, *args, **kwargs):
-        super(ExAxes, self).__init__(*args, **kwargs)
+    Returns a figure object with as much customization as provided.
+    """
+    figargs = {} if figargs is None else figargs
+    if projection == '3d':
+        print('3d axis')
+        fig = sns.mpl.pyplot.figure(**figargs)
+        axs = fig.add_subplot(111, projection=projection)
+    else:
+        print('2d axis')
+        fig, axs = sns.mpl.pyplot.subplots(nyplot, nxplot, **figargs)
+        if joinx:
+            fig.subplots_adjust(wspace=0)
+        if joiny:
+            fig.subplots_adjust(hspace=0)
+    if not isinstance(axs, np.ndarray): axs = np.array([axs])
+    print(type(axs), len(axs))
+    methods = {(x, nxlabel): (axs[0].set_xlim, axs[0].set_xticks),
+               (y, nylabel): (axs[0].set_ylim, axs[0].set_yticks)}
+    print(methods)
+    if projection == '3d':
+        methods.update({(z, nzlabel): (axs[0].set_zlim, axs[0].set_zticks)})
+    for (cart, nlabel), (lim, ticks) in methods.items():
+        print(cart, nlabel, lim, ticks)
+        if cart is not None:
+            lim((cart.min(), cart.max()))
+            if nlabel is not None:
+                ticks(np.linspace(cart.min(), cart.max(), nlabel))
+    return fig
