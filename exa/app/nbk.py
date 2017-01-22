0c174e5ed5f6390608314e629ea6e5f17e2bbac5
Alex Marchenko <alexvmarch@gmail.com>
2017-01-21 23:14:15 -0800

    Commented out old code

--- exa/app/nbk.py
+++ exa/app/nbk.py
@@ -7,15 +7,16 @@ Exa Notebook App
 This module provides a wrapper around the `Jupyter notebook`_ that creates an
 indexed and searchable set of notebook files based on :mod:`~exa.cms.__init__`.
 """
-from notebook.notebookapp import NotebookApp
-from exa._config import config
-
-
-# TODO: Add custom styling to this notebook
-# TODO: Add custom indexing of notebooks related to projects/
-class ExaNotebook(NotebookApp):
-    """Launch Exa notebook in the exa directory."""
-    @classmethod
-    def launch_instance(self, **kwargs):
-        argv = [config['paths']['notebooks']]
-        super(ExaNotebook, self).launch_instance(argv, **kwargs)
+#from notebook.notebookapp import NotebookApp
+#from exa._config import config
+#
+#
+## TODO: Add custom styling to this notebook
+## TODO: Add custom indexing of notebooks related to projects/
+#class ExaNotebook(NotebookApp):
+#    """Launch Exa notebook in the exa directory."""
+#    @classmethod
+#    def launch_instance(self, **kwargs):
+#        argv = [config['paths']['notebooks']]
+#        super(ExaNotebook, self).launch_instance(argv, **kwargs)
+#
