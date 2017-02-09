769fb3d6046d160281ce6929a032dea12d7450cf
Alex Marchenko <alexvmarch@gmail.com>
2017-02-08 21:49:15 -0800

    Testing py3 build

--- js/rst.bat
+++ js/rst.bat
@@ -1,5 +1,6 @@
 REM Helper to build reStructuredText (RST) files using jsdoc
 REM Run from the js\ directory
 REM Run 'npm install' first if necessary
-call node_modules\.bin\jsdoc -t .\node_modules\jsdoc-sphinx\template\ -d ..\docs\source\js\jsdoc_rst\  src\exa-abcwidgets.js
-call rm ..\docs\source\js\jsdoc_rst\index.rst ..\docs\source\js\jsdoc_rst\conf.py
+IF NOT EXIST node_modules\jsdoc-sphinx mkdir node_modules & git clone https://github.com/HumanBrainProject/jsdoc-sphinx node_modules/jsdoc-sphinx
+CALL node_modules\.bin\jsdoc -t node_modules\jsdoc-sphinx\template\ -d ..\docs\source\js\jsdoc_rst\  src\exa-abcwidgets.js
+CALL rm ..\docs\source\js\jsdoc_rst\index.rst ..\docs\source\js\jsdoc_rst\conf.py
--- js/rst.sh
+++ js/rst.sh
@@ -2,5 +2,10 @@
 # Helper to build reStructuredText (RST) files using jsdoc
 # Run from the js/ directory
 # Run 'npm install' first if necessary
-./node_modules/.bin/jsdoc -t node_modules/jsdoc-sphinx/ -d ../docs/source/js/jsdoc_rst/ -r src/
+if [[ ! -d node_modules/jsdoc-sphinx ]]
+then
+    mkdir -p node_modules
+    git clone https://github.com/HumanBrainProject/jsdoc-sphinx node_modules/jsdoc-sphinx
+fi
+./node_modules/.bin/jsdoc -t node_modules/jsdoc-sphinx/template/ -d ../docs/source/js/jsdoc_rst/ -r src/
 rm ../docs/source/js/jsdoc_rst/index.rst ../docs/source/js/jsdoc_rst/conf.py
