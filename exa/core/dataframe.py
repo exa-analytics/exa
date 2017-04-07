38cb704322aae8fc7f593a1829a91679e8049fbe
Alex Marchenko <alexvmarch@gmail.com>
2017-04-07 14:34:55 -0700

    DataFrame start WIP

---
+++ exa/core/dataframe.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015-2017, Exa Analytics Development Team
+# Distributed under the terms of the Apache License 2.0
+"""
+DataFrame
+########################
+"""
+import six
+import pandas as pd
+from .base import ABCBaseMeta, ABCBase
+from .dataseries import DataSeries
+
+
+class DataFrameMeta(ABCBaseMeta):
+    dimensions = (list, tuple)
+
+
+class DataFrame(six.with_metaclass(DataFrameMeta, pd.DataFrame, ABCBase)):
+    _metadata = ["name", "dimensions", "uid", "meta"]
+    _constructor_sliced = DataSeries
+
+    @property
+    def _constructor(self):
+        return DataFrame
---
+++ exa/core/tests/test_dataframe.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015-2017, Exa Analytics Development Team
+# Distributed under the terms of the Apache License 2.0
+"""
+Tests for :mod:`~exa.core.dataframe`
+#############################################
+"""
+from unittest import TestCase
+from exa.core.dataframe import DataFrame
+
+
+class TestDataFrame(TestCase):
+    """Ensure behavior of DataFrame mimics pandas."""
+    pass
