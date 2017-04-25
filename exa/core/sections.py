37032ad60c5a09514233815352dff6b081915770
Alex Marchenko <alexvmarch@gmail.com>
2017-04-24 22:28:22 -0400

    Fixing sections recursions

--- exa/core/sections.py
+++ exa/core/sections.py
@@ -165,7 +165,7 @@ class Sections(six.with_metaclass(SectionsMeta, Editor)):
     """
     name = None        # Subclasses may set this if needed
     description = None # ditto
-    _key_attr_prefix = "section"
+    _section_name_prefix = "section"
 
     @abstractmethod
     def _parse(self):
@@ -194,7 +194,7 @@ class Sections(six.with_metaclass(SectionsMeta, Editor)):
 
     def _gen_sec_attr_name(self, i):
         """Generate section attribute name (e.g. section001)."""
-        return self._key_sec_prefix + str(i).zfill(self._nsections)
+        return self._section_name_prefix + str(i).zfill(len(str(len(self._sections))))
 
     def parse(self, verbose=False):
         """
@@ -212,7 +212,6 @@ class Sections(six.with_metaclass(SectionsMeta, Editor)):
         # First perform the file specific parse method
         self._parse()
         # Now generate section attributes for the sections present
-        self._nsections = len(str(len(self.sections)))
         for i, sec in enumerate(self.sections):
             section = sec[0]
             if section not in self._parsers:
@@ -250,7 +249,7 @@ class Sections(six.with_metaclass(SectionsMeta, Editor)):
             and numbers, and section parsers, respectively.
         """
         self.parse(verbose=verbose)
-        for i in range(self._nsections):
+        for i in range(len(self.sections)):
             self.parse_section(i, recursive=True, verbose=verbose)
 
     def parse_section(self, number, recursive=False, verbose=False):
@@ -275,7 +274,7 @@ class Sections(six.with_metaclass(SectionsMeta, Editor)):
             if verbose:
                 warnings.warn("No parser for section '{}'!".format(section))
             return
-        secname = self._key_sec_prefix + str(number).zfill(self._nsections)
+        secname = self._gen_sec_attr_name(number)
         # Note that we don't actually parse anything until a value is in fact
         # request, e.g. sections.parser.dataobj
         sec = self._parsers[section](self[start:end], path_check=False)
@@ -293,12 +292,12 @@ class Sections(six.with_metaclass(SectionsMeta, Editor)):
         """Retrieve a section by name or number."""
         n = len(str(len(self.sections)))
         if isinstance(i, int):
-            name = self._key_sec_prefix + str(i).zfill(n)
+            name = self._section_name_prefix + str(i).zfill(n)
         else:
             name = []
             for j, nam in enumerate(self.sections):
                 if i in nam:
-                    name.append(self._key_sec_prefix + str(j).zfill(n))
+                    name.append(self._section_name_prefix + str(j).zfill(n))
             if len(name) > 1:
                 raise ValueError("Multiple sections with name = {}".format(i))
             name = name[0]
