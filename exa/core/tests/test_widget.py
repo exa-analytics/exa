a940d42d02ccfd1ea2b086a19a6a06b69d677f65
Alex Marchenko <alexmarc@buffalo.edu>
2017-06-21 16:16:55 -0400

    WIP js

--- exa/core/tests/test_widget.py
+++ exa/core/tests/test_widget.py
@@ -18,7 +18,7 @@ JavaScript representation rather than a static HTML/text representation.
 import os
 import platform
 from unittest import TestCase
-from traitlets import Unicode, Int
+from traitlets import Unicode, List, Int, Bool
 from subprocess import check_call
 from exa.core.container import Container
 from exa.core.widget import DOMWidget, register
@@ -30,74 +30,95 @@ cwd = os.path.dirname(os.path.abspath(__file__))
 nbpath = os.path.join(cwd, nbname)
 tmppath = os.path.join(cwd, "test_widget.ipynb.output")
 htmlpath = os.path.join(cwd, nbname.replace(".ipynb", ".html"))
-expected = ["('Hello World!', 0)",
-            "('Hello', 1)",
-            "('Hello World!', 0)",
-            "('Hello', 1)"]
 
 
-#@register                          # ipywidgets 7.x
-@register("jupyter-exa.TestDOMWidget")    # ipywidgets 6.x
-class TestDOMWidget(DOMWidget):
-    """
-    A test widget that displays text and tests bidirectional communication
-    between Python and JavaScript.
-    """
-    _view_name = Unicode("TestDOMWidgetView").tag(sync=True)
-    _model_name = Unicode("TestDOMWidgetModel").tag(sync=True)
-    frontend_text = Unicode("Hello World!").tag(sync=True)
-    #backend_counter = Int(0).tag(sync=True)
+class TestMSG(DOMWidget):
+    _view_name = Unicode("TestMSGView").tag(sync=True)
+    _model_name = Unicode("TestMSGModel").tag(sync=True)
+    switch = Bool(False).tag(sync=True)
+    telephone = List(trait=Int).tag(sync=True)
 
     def handle_msg(self, *args, **kwargs):
-        self.received = True
+        print("msg received")
+        print(args)
+        print(kwargs)
 
     def __init__(self, *args, **kwargs):
-        super(TestDOMWidget, self).__init__(*args, **kwargs)
-        self.received = False
+        super(TestMSG, self).__init__(*args, **kwargs)
+        self.telephone = [0]
+
+
+
+
+
+#expected = ["('Hello World!', 0)",
+#            "('Hello', 1)",
+#            "('Hello World!', 0)",
+#            "('Hello', 1)"]
 
 
 #@register                          # ipywidgets 7.x
-@register("jupyter-exa.TestKontainer")    # ipywidgets 6.x
-class TestKontainer(Container, TestDOMWidget):
-    """Widget container."""
-    _view_name = Unicode("TestKontainerView").tag(sync=True)
-    _model_name = Unicode("TestKontainerModel").tag(sync=True)
+#@register("jupyter-exa.TestDOMWidget")    # ipywidgets 6.x
+#class TestDOMWidget(DOMWidget):
+#    """
+#    A test widget that displays text and tests bidirectional communication
+#    between Python and JavaScript.
+#    """
+#    _view_name = Unicode("TestDOMWidgetView").tag(sync=True)
+#    _model_name = Unicode("TestDOMWidgetModel").tag(sync=True)
+#    frontend_text = Unicode("Hello World!").tag(sync=True)
+#    #backend_counter = Int(0).tag(sync=True)
+#
+#    def handle_msg(self, *args, **kwargs):
+#        self.received = True
+#
+#    def __init__(self, *args, **kwargs):
+#        super(TestDOMWidget, self).__init__(*args, **kwargs)
+#        self.received = False
+#
+#
+##@register                          # ipywidgets 7.x
+#@register("jupyter-exa.TestKontainer")    # ipywidgets 6.x
+#class TestKontainer(Container, TestDOMWidget):
+#    """Widget container."""
+#    _view_name = Unicode("TestKontainerView").tag(sync=True)
+#    _model_name = Unicode("TestKontainerModel").tag(sync=True)
 
 
-class TestWidgets(TestCase):
-    """
-    Since command line unittests don't check for widget visibility, this test
-    checks for roundtrip messages going from Python to JavaScript and back.
-    """
-    def setUp(self):
-        """Execute the notebook."""
-        try:
-            check_call(["jupyter", "nbconvert", "--exec",
-                        "--ExecutePreprocessor.kernel_name=python", nbpath], cwd=cwd, **prckws)
-        except AssertionError:
-            try:
-                check_call(["jupyter", "nbconvert", "--exec",
-                            "--ExecutePreprocessor.kernel_name=python2", nbpath], cwd=cwd, **prckws)
-            except AssertionError:
-                try:
-                    check_call(["jupyter", "nbconvert", "--exec",
-                                "--ExecutePreprocessor.kernel_name=python3", nbpath], cwd=cwd, **prckws)
-                except Exception as e:
-                    self.fail(msg=str(e))
-
-    def test_value_check(self):
-        """Check bidirectional communication."""
-        try:
-            with open(tmppath) as f:
-                result = [line.strip() for line in f.readlines()]
-            #self.assertListEqual(result, expected)
-        except Exception as e:
-            self.fail(msg=str(e))
-
-    def tearDown(self):
-        """Cleanup the generated file."""
-        try:
-            os.remove(tmppath)
-            os.remove(htmlpath)
-        except Exception as e:
-            self.fail(msg=str(e))
+#class TestWidgets(TestCase):
+#    """
+#    Since command line unittests don't check for widget visibility, this test
+#    checks for roundtrip messages going from Python to JavaScript and back.
+#    """
+#    def setUp(self):
+#        """Execute the notebook."""
+#        try:
+#            check_call(["jupyter", "nbconvert", "--exec",
+#                        "--ExecutePreprocessor.kernel_name=python", nbpath], cwd=cwd, **prckws)
+#        except AssertionError:
+#            try:
+#                check_call(["jupyter", "nbconvert", "--exec",
+#                            "--ExecutePreprocessor.kernel_name=python2", nbpath], cwd=cwd, **prckws)
+#            except AssertionError:
+#                try:
+#                    check_call(["jupyter", "nbconvert", "--exec",
+#                                "--ExecutePreprocessor.kernel_name=python3", nbpath], cwd=cwd, **prckws)
+#                except Exception as e:
+#                    self.fail(msg=str(e))
+#
+#    def test_value_check(self):
+#        """Check bidirectional communication."""
+#        try:
+#            with open(tmppath) as f:
+#                result = [line.strip() for line in f.readlines()]
+#            #self.assertListEqual(result, expected)
+#        except Exception as e:
+#            self.fail(msg=str(e))
+#
+#    def tearDown(self):
+#        """Cleanup the generated file."""
+#        try:
+#            os.remove(tmppath)
+#            os.remove(htmlpath)
+#        except Exception as e:
+#            self.fail(msg=str(e))
--- js/src/tests/test_widget.js
+++ js/src/tests/test_widget.js
@@ -4,88 +4,136 @@
  * Tests for Base Widgets
  */
 "use strict";
-var QUnit = require("qunitjs");
+//var QUnit = require("qunitjs");
 var _ = require("underscore");
 var widget = require("../widget.js");
 
 
-/** Model class for testing bidirectional communication. */
-class TestDOMWidgetModel extends widget.DOMWidgetModel {
-    /**
-     * Get the default class values.
-     * Used by jupyter-js-widgets.
-     */
-    get defaults() {
-        return _.extend({}, widget.DOMWidgetModel.prototype.defaults, {
-            _view_name: "TestDOMWidgetView",
-            _model_name: "TestDOMWidgetModel",
-            frontend_text: "Hello World!",
-            backend_counter: 0
-        });
-    }
-}
-
-
-/** View class for testing bidirectional communication. */
-class TestDOMWidgetView extends widget.DOMWidgetView {
-    /**
-     * Render the view in the notebook.
-     * Call order it initialize, constructor, render.
-     */
+class TestMSGView extends widget.DOMWidgetView {
     render() {
-        this.set_frontend();
-        this.listenTo(this.model, "change:frontend_text", this.change, this);
+        console.log("rendering");
+        this.listenTo(this.model, "change:switch", this.change, this);
     }
 
-    /**
-     * Set the value of the widget text
-     */
     set_frontend() {
-        this.el.textContent = this.model.get("frontend_text");
+        this.el.textContent = this.model.get("switch");
     }
 
-    /**
-     * Call front end setter and then backend incrementer.
-     */
     change() {
         this.set_frontend();
-        this.inc_backend();
+        this.inc_list();
     }
 
-    /**
-     * Increment backend counter and set the model.
-     */
-    inc_backend() {
-        var bc = this.model.get("backend_counter");
-        bc++;
-        this.model.set("backend_counter", bc);
-        this.model.save_changes();
-        //this.touch();
+    inc_list() {
+        var list = this.model.get("telephone");
+        var last = list[list.length-1];
+        last++;
+        list.push(last);
+        this.model.set("telephone", list);
+        this.touch();
+        this.send({"tele": list});
+    }
+
+    on_msg(args) {
+        console.log("msg received");
+        console.log(args);
     }
 }
 
 
-/** Mock model for exa.core.tests.test_widget.Kontainer */
-class TestKontainerModel extends TestDOMWidgetModel {
-    /**
-     * Get the default class values.
-     */
+class TestMSGModel extends widget.DOMWidgetModel {
     get defaults() {
-        return _.extend({}, TestDOMWidgetModel.prototype.defaults, {
-            _view_name: "TestKontainerView",
-            _model_name: "TestKontainerModel",
+        return _.extend({}, widget.DOMWidgetModel.prototype.defaults, {
+            _view_name: "TestMSGView",
+            _model_name: "TestMSGModel",
+            telephone: [0]
         });
     }
 }
-
-/** Mock view for exa.core.tests.test_widget.Kontainer */
-class TestKontainerView extends TestDOMWidgetView {}
-
 
 
 module.exports = {
-    TestDOMWidgetModel: TestDOMWidgetModel,
-    TestDOMWidgetView: TestDOMWidgetView,
-    TestKontainerModel: TestKontainerModel,
-    TestKontainerView: TestKontainerView
+    TestMSGModel: TestMSGModel,
+    TestMSGView: TestMSGView
 };
+
+///** Model class for testing bidirectional communication. */
+//class TestDOMWidgetModel extends widget.DOMWidgetModel {
+//    /**
+//     * Get the default class values.
+//     * Used by jupyter-js-widgets.
+//     */
+//    get defaults() {
+//        return _.extend({}, widget.DOMWidgetModel.prototype.defaults, {
+//            _view_name: "TestDOMWidgetView",
+//            _model_name: "TestDOMWidgetModel",
+//            frontend_text: "Hello World!",
+//            backend_counter: 0
+//        });
+//    }
+//}
+//
+//
+///** View class for testing bidirectional communication. */
+//class TestDOMWidgetView extends widget.DOMWidgetView {
+//    /**
+//     * Render the view in the notebook.
+//     * Call order it initialize, constructor, render.
+//     */
+//    render() {
+//        this.set_frontend();
+//        this.listenTo(this.model, "change:frontend_text", this.change, this);
+//    }
+//
+//    /**
+//     * Set the value of the widget text
+//     */
+//    set_frontend() {
+//        this.el.textContent = this.model.get("frontend_text");
+//    }
+//
+//    /**
+//     * Call front end setter and then backend incrementer.
+//     */
+//    change() {
+//        this.set_frontend();
+//        this.inc_backend();
+//    }
+//
+//    /**
+//     * Increment backend counter and set the model.
+//     */
+//    inc_backend() {
+//        var bc = this.model.get("backend_counter");
+//        bc++;
+//        this.model.set("backend_counter", bc);
+//        this.model.save_changes();
+//        //this.touch();
+//    }
+//}
+//
+//
+///** Mock model for exa.core.tests.test_widget.Kontainer */
+//class TestKontainerModel extends TestDOMWidgetModel {
+//    /**
+//     * Get the default class values.
+//     */
+//    get defaults() {
+//        return _.extend({}, TestDOMWidgetModel.prototype.defaults, {
+//            _view_name: "TestKontainerView",
+//            _model_name: "TestKontainerModel",
+//        });
+//    }
+//}
+//
+///** Mock view for exa.core.tests.test_widget.Kontainer */
+//class TestKontainerView extends TestDOMWidgetView {}
+//
+//
+//
+//module.exports = {
+//    TestDOMWidgetModel: TestDOMWidgetModel,
+//    TestDOMWidgetView: TestDOMWidgetView,
+//    TestKontainerModel: TestKontainerModel,
+//    TestKontainerView: TestKontainerView
+//};
