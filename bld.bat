fd913e56b1d52ba0cde3e65bc49d1db5f0b9929e
Alex Marchenko <alexvmarch@gmail.com>
2017-06-21 19:42:17 -0400

    Updating win build

--- bld.bat
+++ bld.bat
@@ -1,4 +1,4 @@
 "%PYTHON%" setup.py install
-jupyter nbextension install exatomic --py --sys-prefix --overwrite
-jupyter nbextension enable exatomic --py --sys-prefix
+jupyter nbextension install exa --py --sys-prefix --overwrite
+jupyter nbextension enable exa --py --sys-prefix
 if errorlevel 1 exit 1
