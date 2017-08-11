2276db619300eefe3bca3d3b39cf2458a889cf8f
Alex Marchenko <alexmarc@buffalo.edu>
2017-08-10 21:42:14 -0400

    apibuilder

--- js/src/apibuilder.js
+++ js/src/apibuilder.js
@@ -14,7 +14,9 @@ var modules = require("./imports.js");
 class APIBuilderView extends base.WidgetView {
     render() {
         console.log("here");
+        this.exports = {};
         this.build();
+        console.log(this.exports);
     }
 
     /**
@@ -23,9 +25,9 @@ class APIBuilderView extends base.WidgetView {
     build() {
         console.log("building...");
         console.log(modules);
-        for (var module in modules) {
-            if (modules.hasOwnProperty(module)) {
-                this.analyze_module(module);
+        for (var modname in modules) {
+            if (modules.hasOwnProperty(modname)) {
+                this.analyze_module(modname, modules[modname]);
             }
         }
     }
@@ -34,14 +36,54 @@ class APIBuilderView extends base.WidgetView {
      * Analyze a module, extracting function names and
      * arguments in order to build an API.
      */
-    analyze_module(module) {
+    analyze_module(modname, module) {
         console.log("analyzing...", module);
         var names = Object.getOwnPropertyNames(module);
         var n = names.length;
+        this.exports[modname] = {};
+        console.log(n);
         for (var i = 0; i < n; i++) {
             var name = names[i];
-            console.log(name);
+            var attr = module[name];
+            var type = typeof attr;
+            if (type === "function") {
+                var view_name = name + "View";
+                var model_name = name + "Model";
+                var argnames = this.get_arg_names(attr);
+                var modelcls = class extends base.DOMWidgetModel {
+                    defaults() {
+                        return _.extend({}, base.DOMWidgetModel.prototype.defaults(), {
+                            _view_name: view_name,
+                            _model_name: model_name
+                        });
+                    }
+                };
+                var viewcls = class extends base.DOMWidgetView {
+                    render() {
+                        var args;
+                        var n = argnames.length;
+                        for (var i = 0; i < n; i++) {
+                            var name = argnames[i];
+                            args[name] = this.model.get(name);
+                        }
+                        this.args = args;
+                        this.obj = new this.func(this.args);
+                        this.el = this.obj;
+                    }
+                };
+                this.exports[modname][view_name] = viewcls;
+                this.exports[modname][model_name] = modelcls;
+            }
         }
+    }
+
+    get_arg_names(func) {
+        var strfn = func.toString().replace(/((\/\/.*$)|(\/\/*[\s\S]*?\*\/))/mg, "");
+        var result = strfn.slice(strfn.indexOf("(") + 1, strfn.indexOf(")")).match(/([^\s,]+)/g);
+        if (result === null) {
+            result = [];
+        }
+        return result;
     }
 }
 
