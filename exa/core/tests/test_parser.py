1e4ede8076a8194170f1fc20e0cdbd006113fda2
Alex Marchenko <alexmarc@buffalo.edu>
2017-09-24 20:05:12 -0400

    WIP parser

--- exa/core/tests/test_parser.py
+++ exa/core/tests/test_parser.py
@@ -7,41 +7,41 @@ Tests for :mod:`~exa.core.parser`
 Test the parsing engine on this file.
 """
 from unittest import TestCase
-from exa.core.parser import Parser
+#from exa.core.parser import Parser
 
 
-class SrcParser(Parser):
-    """Top level parser for Python source code (testing)."""
-    def _parse(self):
-        """Only parses auxiliary text at the top of files."""
-        pass
-
-
-class DocString(Parser):
-    """Docstring parser."""
-    def _parse(self):
-        """Parse docstrings."""
-        df = list(self.find('"""', "'''").all().numpairs())
-
-
-class PyFunc(Parser):
-    """
-    """
-    pass
-
-
-class PyClass(Parser):
-    """
-    """
-    pass
-
-
-class PyDocString(Parser):
-    """
-    """
-    pass
-
-
-class TestParser(TestCase):
-    """Source code is a trivial example of a semi-structured file."""
-    pass
+#class SrcParser(Parser):
+#    """Top level parser for Python source code (testing)."""
+#    def _parse(self):
+#        """Only parses auxiliary text at the top of files."""
+#        pass
+#
+#
+#class DocString(Parser):
+#    """Docstring parser."""
+#    def _parse(self):
+#        """Parse docstrings."""
+#        df = list(self.find('"""', "'''").all().numpairs())
+#
+#
+#class PyFunc(Parser):
+#    """
+#    """
+#    pass
+#
+#
+#class PyClass(Parser):
+#    """
+#    """
+#    pass
+#
+#
+#class PyDocString(Parser):
+#    """
+#    """
+#    pass
+#
+#
+#class TestParser(TestCase):
+#    """Source code is a trivial example of a semi-structured file."""
+#    pass
