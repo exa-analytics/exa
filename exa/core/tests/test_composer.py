7a7763a37dcc4d522e8ce22ee81e026997d1be18
Alex Marchenko <alexmarc@buffalo.edu>
2017-10-03 19:30:05 -0400

    Start of composer tests

---
+++ exa/core/tests/test_composer.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015-2017, Exa Analytics Development Team
+# Distributed under the terms of the Apache License 2.0
+"""
+Tests for :mod:`~exa.core.composer`
+#############################################
+"""
+from unittest import TestCase
+from exa.core.composer import Composer
+
+
+template = """This is my
+[one|,[
+"""
+
+
+class TestComposer(TestCase):
+    pass
