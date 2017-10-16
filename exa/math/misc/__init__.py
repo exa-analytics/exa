ecc24660c8a72f8a4f680172fb1c003f6653b4fd
Alex Marchenko <alexmarc@buffalo.edu>
2017-10-16 19:21:54 -0400

    Strip out math which itself is migrated to projects where it is required.

--- exa/math/misc/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2015-2017, Exa Analytics Development Team
-# Distributed under the terms of the Apache License 2.0
-from .indexing import starts_count, starts_counts
-from .repeat import repeat_count, repeat_counts_f8_1d, repeat_counts_f8_2d
-from .summation import (sum_product_pair, sum_product_pair_f8,
-                        sum_product_pair_obj)
--- exa/math/vector/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2015-2017, Exa Analytics Development Team
-# Distributed under the terms of the Apache License 2.0
-from .cartesian import (magnitude_xyz, magnitude_xyz_squared,
-                        pdist_euclidean, pdist_euclidean_dr,
-                        pdist_euc_dxyz_idx, periodic_pdist_euc_dxyz_idx)
