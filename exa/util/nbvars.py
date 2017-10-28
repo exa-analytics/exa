156cbe988249b4f6e1d4ef67624a8ef967fc458d
Alex Marchenko <alexvmarch@gmail.com>
2017-10-27 21:20:17 -0400

    Now warn when not using signatures for vec numbafy

--- exa/util/nbvars.py
+++ exa/util/nbvars.py
@@ -20,6 +20,7 @@ import six
 import numpy as np
 import sympy as sy
 import numba as nb
+from warnings import warn
 from platform import system
 from sympy.utilities.lambdify import NUMPY_TRANSLATIONS, NUMPY_DEFAULT
 
@@ -39,10 +40,10 @@ def numbafy(fn, args, compiler="jit", **nbkws):
     """
     Compile a string, sympy expression or symengine expression using numba.
 
-    Note:
-        Not all functions are supported by Python's numerical package (numpy).
-        For difficult cases, valid Python code (as string) may be more suitable
-        than symbolic expressions coming from sympy, symengine, etc.
+    Not all functions are supported by Python's numerical package (numpy). For
+    difficult cases, valid Python code (as string) may be more suitable than
+    symbolic expressions coming from sympy, symengine, etc. When compiling
+    vectorized functions, include valid signatures (see `numba`_ documentation).
 
     Args:
         fn: Symbolic expression as sympy/symengine expression or string
@@ -54,9 +55,7 @@ def numbafy(fn, args, compiler="jit", **nbkws):
         func: Compiled function
 
     Warning:
-        The argument **nbkws** will override default options. The **cache**
-        keyword argument is not guaranteed to be respected, especially when
-        working in a dynamic/command line environment.
+        For vectorized functions, valid signatures are (almost always) required.
     """
     kwargs = {}    # Numba kwargs to be updated by user
     if not isinstance(args, (tuple, list)):
@@ -69,8 +68,12 @@ def numbafy(fn, args, compiler="jit", **nbkws):
         compiler = compiler_
     if compiler in (nb.jit, nb.njit, nb.autojit):
         kwargs.update(jitkwargs)
+        sig = nbkws.pop("signature", None)
     else:
         kwargs.update(veckwargs)
+        sig = nbkws.pop("signatures", None)
+        if sig is None:
+            warn("Vectorization without 'signatures' can lead to wrong results!")
     kwargs.update(nbkws)
     # Expand sympy expressions and create string for eval
     if isinstance(fn, sy.Expr):
--- exa/util/tests/test_nbvars.py
+++ exa/util/tests/test_nbvars.py
@@ -20,58 +20,52 @@ class TestNumbafy(TestCase):
     """
     def setUp(self):
         self.sca = 0.1
-        self.arr = np.random.rand(10)
+        self.arr = np.random.rand(10).astype("float32")
+        self.sig = ["float32(float32)"]
 
     def test_simple_strings(self):
         """Test string functions."""
         fn = "sin(x)/x"
-        func = numbafy(fn, "x", compiler="vectorize")
-        import warnings
-        warnings.warn(type(func))
-        warnings.warn(func.__doc__)
-        warnings.warn(type(self.sca))
-        warnings.warn(type(self.arr))
-        warnings.warn(self.arr)
-
+        func = numbafy(fn, "x", compiler="vectorize", signatures=self.sig)
         self.assertTrue(np.allclose(func(self.arr),
                                     np.sin(self.arr)/self.arr))
         func = numbafy(fn, "x", compiler="jit")
         self.assertTrue(np.isclose(func(self.sca),
                                    np.sin(self.sca)/self.sca))
 
-#    def test_fail_string(self):
-#        """Test failure on untyped name."""
-#        fn = "Sin(x)/x"
-#        func = numbafy(fn, "x")
-#        with self.assertRaises((TypingError, NameError)):
-#            func(self.sca)
-#
-#    def test_complex_strings(self):
-#        """Test more complicated string functions."""
-#        fn = "arccos(x)/y + exp(-y) + mod(z, 2)"
-#        func = numbafy(fn, ("x", "y", "z"), compiler="vectorize")
-#        result = func(self.arr, self.arr, self.arr)
-#        check = np.arccos(self.arr)/self.arr + np.exp(-self.arr) + np.mod(self.arr, 2)
-#        self.assertTrue(np.allclose(result, check))
-#        func = numbafy(fn, ("x", "y", "z"))
-#        result = func(self.sca, self.sca, self.sca)
-#        check = np.arccos(self.sca)/self.sca + np.exp(-self.sca) + np.mod(self.sca, 2)
-#        self.assertTrue(np.isclose(result, check))
-#
-#    def test_sympy(self):
-#        """Test sympy expressions."""
-#        x, y, z = sy.symbols("x y z")
-#        fn = sy.acos(x)/y + sy.exp(-y) + sy.Mod(z, 2)
-#        func = numbafy(fn, (x, y, z), compiler="vectorize")
-#        result = func(self.arr, self.arr, self.arr)
-#        check = np.arccos(self.arr)/self.arr + np.exp(-self.arr) + np.mod(self.arr, 2)
-#        self.assertTrue(np.allclose(result, check))
-#
-#    def test_symengine(self):
-#        """Test symengine."""
-#        x, y, z = sge.var("x y z")
-#        fn = sge.acos(x)/y + sge.exp(-z)
-#        func = numbafy(fn, (x, y, z), compiler="vectorize")
-#        result = func(self.arr, self.arr, self.arr)
-#        check = np.arccos(self.arr)/self.arr + np.exp(-self.arr)
-#        self.assertTrue(np.allclose(result, check))
+    def test_fail_string(self):
+        """Test failure on untyped name."""
+        fn = "Sin(x)/x"
+        func = numbafy(fn, "x")
+        with self.assertRaises((TypingError, NameError)):
+            func(self.sca)
+
+    def test_complex_strings(self):
+        """Test more complicated string functions."""
+        fn = "arccos(x)/y + exp(-y) + mod(z, 2)"
+        func = numbafy(fn, ("x", "y", "z"), compiler="vectorize", signatures=self.sig)
+        result = func(self.arr, self.arr, self.arr)
+        check = np.arccos(self.arr)/self.arr + np.exp(-self.arr) + np.mod(self.arr, 2)
+        self.assertTrue(np.allclose(result, check))
+        func = numbafy(fn, ("x", "y", "z"))
+        result = func(self.sca, self.sca, self.sca)
+        check = np.arccos(self.sca)/self.sca + np.exp(-self.sca) + np.mod(self.sca, 2)
+        self.assertTrue(np.isclose(result, check))
+
+    def test_sympy(self):
+        """Test sympy expressions."""
+        x, y, z = sy.symbols("x y z")
+        fn = sy.acos(x)/y + sy.exp(-y) + sy.Mod(z, 2)
+        func = numbafy(fn, (x, y, z), compiler="vectorize", signatures=self.sig)
+        result = func(self.arr, self.arr, self.arr)
+        check = np.arccos(self.arr)/self.arr + np.exp(-self.arr) + np.mod(self.arr, 2)
+        self.assertTrue(np.allclose(result, check))
+
+    def test_symengine(self):
+        """Test symengine."""
+        x, y, z = sge.var("x y z")
+        fn = sge.acos(x)/y + sge.exp(-z)
+        func = numbafy(fn, (x, y, z), compiler="vectorize", signatures=self.sig)
+        result = func(self.arr, self.arr, self.arr)
+        check = np.arccos(self.arr)/self.arr + np.exp(-self.arr)
+        self.assertTrue(np.allclose(result, check))
