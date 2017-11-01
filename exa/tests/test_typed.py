06f1fa633ed4e1dbbb52320a430b5e3a54dc6bb2
Alex Marchenko <alexvmarch@gmail.com>
2017-10-31 20:02:40 -0400

    Updated typed tests using pytest decreasing the lines by half while maintaing coverage

--- exa/tests/test_typed.py
+++ exa/tests/test_typed.py
@@ -9,7 +9,7 @@ See :mod:`~exa.typed`
 import six
 import pytest
 from itertools import product
-from exa.typed import Typed, typed, TypedClass, TypedMeta
+from exa.typed import Typed, typed, TypedClass, TypedMeta, yield_typed
 
 
 @typed
@@ -48,6 +48,7 @@ def test_simple(simple):
         simple.foo = "forty two"
     del simple._foo
     assert simple.foo is None
+    assert len(list(yield_typed(simple))) == 1
 
 
 # Complex example
@@ -55,12 +56,26 @@ def pre_set(obj):
     obj.pre_set_called = True
 
 
+def pre_del(obj):
+    obj.pre_del_called = True
+
+
 class Complete(TypedClass):
     """Test advanced usage."""
     foo = Typed(int, doc="Test documentation", autoconv=False, allow_none=False,
-                pre_set=pre_set)
+                pre_set=pre_set, pre_get="pre_get", pre_del=pre_del, post_del="post_del")
+
+    def pre_get(self):
+        self.pre_get_called = True
+
+    def post_del(self):
+        self.post_del_called = True
 
     def __init__(self):
+        self.pre_del_called = False
+        self.post_del_called = False
+        self.pre_del_called = False
+        self.pre_get_called = False
         self.pre_set_called = False
 
 
@@ -72,5 +87,37 @@ def cmplx():
 def test_complex(cmplx):
     """Test auxiliary options of Typed attributes."""
     assert cmplx.pre_set_called == False
+    assert cmplx.pre_get_called == False
+    none = cmplx.foo
+    assert none is None
+    assert cmplx.pre_get_called == True
+    assert cmplx.pre_set_called == False
     cmplx.foo = 42
     assert cmplx.pre_set_called == True
+    assert len(list(yield_typed(cmplx))) == 1
+    with pytest.raises(TypeError):
+        cmplx.foo = 42.0
+    assert cmplx.pre_del_called == False
+    assert cmplx.post_del_called == False
+    del cmplx.foo
+    assert cmplx.pre_del_called == True
+    assert cmplx.post_del_called == True
+    assert "Test documentation" in cmplx.__class__.foo.__doc__
+
+
+class Simple4(TypedClass):
+    _setters = ("_set", )
+    foo = Typed(int)
+
+    def _set_foo(self):
+        self.foo = 42
+
+
+@pytest.fixture(scope="session")
+def auto():
+    return Simple4()
+
+
+def test_auto(auto):
+    """Test automatic setting."""
+    assert auto.foo == 42
