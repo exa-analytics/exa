3836ae4f4cf64080611b27e3ca03716874781361
Alex Marchenko <alexmarc@buffalo.edu>
2017-11-01 15:28:08 -0400

    Attempt to fix isotopes reading. Update travis to only check that osx installs. Only build docs from one build

--- exa/util/isotopes.py
+++ exa/util/isotopes.py
@@ -38,7 +38,6 @@ import bz2 as _bz2
 import json as _json
 import six as _six
 import sys as _sys
-from exa import Editor as _Editor
 from exa import DataFrame as _DF
 from exa.static import resource as _resource
 
@@ -144,7 +143,7 @@ def _create():
             setattr(ele, "_"+str(tope.A), tope)
         return ele
 
-    with _bz2.open(_path) as f:
+    with _bz2.open(_path, "rb") as f:
         iso = _DF(_json.load(f), columns=_columns)
     for element in iso.groupby("symbol").apply(creator):
         setattr(_this, element.symbol, element)
