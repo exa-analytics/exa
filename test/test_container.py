1db1d0c0e85030d79a744ef9366511a21b59637b
Alex Marchenko <alexvmarch@gmail.com>
2020-01-31 17:04:58 -0500

    Removed typing from main import

--- test/test_container.py
+++ test/test_container.py
@@ -1,53 +1,16 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2015-2019, Exa Analytics Development Team
+# Copyright (c) 2015-2020, Exa Analytics Development Team
 # Distributed under the terms of the Apache License 2.0
 """
 Tests for :mod:`~exa.core.container`
 #######################################
 """
-import six
-import pandas as pd
-from unittest import TestCase
-from exa import Container, TypedMeta, DataFrame, Series
+from exa import Container
 
 
-class DummyDataFrame(DataFrame):
-    _index = 'index'
-    _categories = {'cat': str}
-    _columns = ['x', 'y', 'z', 'cat']
-
-
-class DummySeries(Series):
-    _precision = 3
-    _sname = 'field'
-    _iname = 'value'
-
-
-class DummyMeta(TypedMeta):
-    s0 = DummySeries
-    s1 = DummySeries
-    df = DummyDataFrame
-
-
-class DummyContainer(six.with_metaclass(DummyMeta, Container)):
-    pass
-
-
-class TestContainer(TestCase):
-    def setUp(self):
-        x = [0, 0, 0, 0, 0]
-        y = [1.1, 2.2, 3.3, 4.4, 5.5]
-        z = [0.5, 1.5, 2.5, 3.5, 4.5]
-        cat = ['cube', 'sphere', 'cube', 'sphere', 'cube']
-        group = [0, 0, 1, 1, 1]
-        self.container = DummyContainer()
-        self.container._test = False
-        self.container.s0 = DummySeries(y)
-        self.container.s1 = DummySeries(cat, dtype='category')
-        self.container.df = pd.DataFrame.from_dict({'x': x, 'y': y, 'z': z, 'cat': cat, 'group': group})
-
-    def test_attributes(self):
-        self.assertIsInstance(self.container.s0, DummySeries)
-        self.assertIsInstance(self.container.s1.dtype, pd.api.types.CategoricalDtype)
-        self.assertIsInstance(self.container.df, DummyDataFrame)
-
+def test_container_constructor():
+    c = Container()
+    assert isinstance(c, Container)
+    assert hasattr(c, "name")
+    assert hasattr(c, "description")
+    assert hasattr(c, "meta")
