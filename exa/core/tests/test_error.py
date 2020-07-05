c120648be7f8014bce5fbe37a4dfcc96c775b7af
Alex Marchenko <alexvmarch@gmail.com>
2020-07-05 15:20:23 -0400

    Remove unused import

--- exa/core/tests/test_error.py
+++ exa/core/tests/test_error.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2015-2020, Exa Analytics Development Team
 # Distributed under the terms of the Apache License 2.0
-import pytest
 from exa.core.error import ExaException, RequiredColumnError
 
 
